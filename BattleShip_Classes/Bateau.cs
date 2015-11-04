113379ad5f3213958f2c6529215d931494ba53f9
Manu <ebeloin@hotmail.com>
2015-11-03 20:49:40 -0500

    Ajout de classes pour gérer les bateaux

---
+++ BattleShip_Classes/Bateau.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace BattleShip_Classes
+{
+    public class Bateau
+    {
+        public string _nom { get; private set; }
+        public List<Point> _position = new List<Point>();
+        public bool[] _estVivant { get; private set; }
+        public int _taille { get; private set; }
+
+        public Bateau(string nom, List<Point> position)
+        {
+            _nom = nom;
+            _position = position;
+            // On affecte la taille du bateau en considérant le nombre de coordonnées
+            _taille = position.Count;
+            // On affecte une booléenne par case du bateau
+            _estVivant = new bool[_taille];
+
+            // On met à true toutes les booléennes du bateau en question
+            for (int i = 0; i < _taille; ++i)
+            {
+                _estVivant[i] = true;
+            }
+        }
+        public bool BateauEstVivant()
+        {
+            int nombreCasesTouchees = 0;
+            bool estVivant = true;
+            for (int i = 0; i < _taille; ++i)
+            {
+                // Si la case a été touchée
+                if (!_estVivant[i])
+                {
+                    nombreCasesTouchees++;
+                }
+            }
+            if (nombreCasesTouchees == _taille)
+            {
+                estVivant = false;
+            }
+            return estVivant;
+        }
+    }
+}
---
+++ BattleShip_Classes/Flotte.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip_Classes
+{
+    class Flotte
+    {
+        public List<Bateau> flotte { get; private set; }
+        
+        public Flotte(List<Bateau> liste)
+        {
+            flotte = new List<Bateau>();
+            flotte = liste;
+        }
+
+        public bool FlotteEstVivante()
+        {
+            bool estVivante = true;
+            int bateauxCoules = 0;
+            // Calcule le nombre de bateaux coulés
+            for(int i = 0; i < flotte.Count; ++i)
+            {
+                if(!flotte[i].BateauEstVivant())
+                {
+                    bateauxCoules++;
+                }
+            }
+            if(bateauxCoules == flotte.Count)
+            {
+                estVivante = false;
+            }
+            return estVivante;
+        }
+    }
+}
